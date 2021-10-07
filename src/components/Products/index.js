9e07d088f023d53a6272e654b296b73a86c012fa
dokidinesh <dinesh.doki@gmail.com>
2021-10-07 12:40:07 +0000

    authentication coding practice

---
+++ src/components/Products/index.js
@@ -0,0 +1,25 @@
+// Write your JS code here
+import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import Header from '../Header'
+import './index.css'
+
+const Products = () => {
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return (
+    <>
+      <Header />
+      <div className="cart-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+          alt="products"
+        />
+      </div>
+    </>
+  )
+}
+
+export default Products
